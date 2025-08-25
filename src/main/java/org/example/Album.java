dc25aa31600a0d0b74487440d0b7f1008c3dc73c
jrueter15 <45775156+jrueter15@users.noreply.github.com>
2025-08-25 18:03:18 -0500

    All tests passed

---
+++ src/main/java/org/example/Album.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Album extends LibraryItem{
+    protected int trackCount;
+
+    public Album(String title, String author, int year, int trackCount){
+        super(title, year, author);
+        this.trackCount = trackCount;
+    }
+
+    public int getTrackCount(){
+        return trackCount;
+    }
+
+    public String toString(){
+        return "Album: " + title + " by " + author + " (" + year + ") - " + trackCount + " tracks";
+    }
+}
